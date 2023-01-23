91be581d858b90664b92ab127f7095830cc67a24
Alexis <alexis@parfaitementweb.com>
2023-01-23 16:32:55 +0100

    Fix missing Contract.

--- src/FilamentTinyMce.php
+++ src/FilamentTinyMce.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace Parfaitementweb\FilamentTinyMce;
-use Filament\Forms\Components\Field;
 
+use Filament\Forms\Components\Field;
 use Filament\Forms\Components\Concerns;
 use Filament\Forms\Components\Contracts;
 
-class FilamentTinyMce extends Field implements Contracts\HasFileAttachments
+class FilamentTinyMce extends Field implements Contracts\CanBeLengthConstrained, Contracts\HasFileAttachments
 {
+    use Concerns\CanBeLengthConstrained;
     use Concerns\HasFileAttachments;
 
     protected string $view = 'filament-tinymce::filament-tinymce';
